e6108e2af67044bc3b2d3e3b760b54af3945f8f8
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-09-13 11:08:22 +0300

    Add variables and args to generate command

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -2,8 +2,10 @@
 
 const version = require(`./version.js`);
 const help = require(`./help.js`);
+const generate = require(`./generate.js`);
 
 module.exports.Cli = {
   [version.name]: version,
   [help.name]: help,
+  [generate.name]: generate,
 };
--- src/service/service.js
+++ src/service/service.js
@@ -4,6 +4,7 @@ const {Cli} = require(`./cli`);
 
 const args = process.argv.slice(2);
 const command = args[0];
+const params = args.slice(1);
 
-Cli[command].run();
+Cli[command].run(params);
 process.exit(0);
