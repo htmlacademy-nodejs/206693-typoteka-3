9d9f1a5b5ba710beb7c10491ee4ec82ab56f9002
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-09-13 18:06:19 +0300

    Add checks for commands

--- src/service/service.js
+++ src/service/service.js
@@ -6,5 +6,17 @@ const args = process.argv.slice(2);
 const command = args[0];
 const params = args.slice(1);
 
+if (!command) {
+  console.log(`No command passed`);
+  Cli[`--help`].run();
+  process.exit(1);
+}
+
+if (!Cli[command]) {
+  console.log(`Unknown command: ${command}`);
+  Cli[`--help`].run();
+  process.exit(1);
+}
+
 Cli[command].run(params);
 process.exit(0);
