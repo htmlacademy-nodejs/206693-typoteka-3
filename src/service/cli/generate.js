6490a243c73017c06efe04c35da818456fe69848
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-10-05 11:56:11 +0300

    Refactoring

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,42 +1,34 @@
 'use strict';
-
 const chalk = require(`chalk`);
-const fsPromises = require(`fs`).promises;
+const {getRandomInt, getRandomDate, readFile, writeFile} = require('../../utils');
 
 const FILE_ANNOUNCE_PATH = `./data/announce.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
-
 const DEFAULT_COUNT = 1;
 const MAX_COUNT = 1000;
 const FILE_NAME = `mocks.json`;
 
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+module.exports = {
+  name: `--generate`,
+  async run(params) {
+    const count = Number(params[0]) || DEFAULT_COUNT;
+    if (count > MAX_COUNT) {
+      throw new Error(chalk.red(`Не больше ${MAX_COUNT} объявлений`));
+    }
 
-const getRandomDate = () => {
-  const toDate = new Date();
-  const fromDate = new Date().setMonth(toDate.getMonth() - 3);
-  const randomDate = new Date(getRandomInt(fromDate, toDate)).toISOString();
+    const publications = await generatePublications(count);
+    const output = formatOutput(publications);
 
-  return `${randomDate.substr(0, 10)} ${randomDate.substr(11, 8)}`
-}
-
-const readContent = async (filePath) => {
-  try {
-    const content = await fsPromises.readFile(filePath, `utf8`);
-    return content.trim().split(`\n`);
-  } catch (err) {
-    console.error(chalk.red(err));
-    return [];
+    await writeFile(FILE_NAME, output);
   }
 };
 
-const generatePublications = (count, titleList, announceList, categoryList) => {
+async function generatePublications(count) {
   const publications = [];
+  const titleList = parseData(await readFile(FILE_TITLES_PATH));
+  const announceList = parseData(await readFile(FILE_ANNOUNCE_PATH));
+  const categoryList = parseData(await readFile(FILE_CATEGORIES_PATH));
 
   for (let i = 0; i < count; i++) {
     const title = titleList[getRandomInt(0, titleList.length - 1)];
@@ -55,24 +47,10 @@ const generatePublications = (count, titleList, announceList, categoryList) => {
   return publications;
 }
 
-module.exports = {
-  name: `--generate`,
-  async run(params) {
-    const count = Number(params[0]) || DEFAULT_COUNT;
-    const titles = await readContent(FILE_TITLES_PATH);
-    const announce = await readContent(FILE_ANNOUNCE_PATH);
+function parseData(source) {
+  return source.trim().split(`\n`);
+}
 
-    const categories = await readContent(FILE_CATEGORIES_PATH);
-
-    if (count > MAX_COUNT) {
-      throw new Error(chalk.red(`Не больше ${MAX_COUNT} объявлений`));
-    }
-    const json = JSON.stringify(generatePublications(count, titles, announce, categories), null, 4);
-    try {
-      await fsPromises.writeFile(FILE_NAME, json);
-      console.log(chalk.green(`Operation success. File created.`));
-    } catch (err) {
-      console.error(chalk.red(`Can't write data to file...`));
-    }
-  }
-};
+function formatOutput(publications) {
+  return JSON.stringify(publications, null, 4);
+}
