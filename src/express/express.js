5bccf93be733dd33349e1faeed08cd5cb7d711ec
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-11-10 18:24:04 +0300

    Register routes

--- src/express/express.js
+++ src/express/express.js
@@ -2,7 +2,16 @@
 
 const express = require(`express`);
 const {DEFAULT_PORT} = require(`../constants`);
-
+const articlesRoutes = require(`./routes/articles-routes`);
+const myRoutes = require(`./routes/my-routes`);
 const app = express();
+
+app.get(`/`, (req, res) => res.send(`/`));
+app.get(`/register`, (req, res) => res.send(`/register`));
+app.get(`/login`, (req, res) => res.send(`/login`));
+app.get(`/search`, (req, res) => res.send(`/search`));
+app.get(`/categories`, (req, res) => res.send(`/categories`));
+app.use(`/articles`, articlesRoutes);
+app.use(`/my`, myRoutes);
 
 app.listen(DEFAULT_PORT);
