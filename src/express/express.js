cacc6ae13fdd23ce624a3fe53ee90788facd901f
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-11-13 17:02:17 +0300

    Add template for main

--- src/express/express.js
+++ src/express/express.js
@@ -12,7 +12,7 @@ app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
-app.get(`/`, (req, res) => res.send(`/`));
+app.get(`/`, (req, res) => res.render(`main`));
 app.get(`/register`, (req, res) => res.send(`/register`));
 app.get(`/login`, (req, res) => res.send(`/login`));
 app.get(`/search`, (req, res) => res.send(`/search`));
