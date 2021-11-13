a60178b8cc8a36d042d380e78f2638b1dbc03af7
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-11-13 17:13:09 +0300

    Add template for sign-up

--- src/express/express.js
+++ src/express/express.js
@@ -12,12 +12,12 @@ app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
-app.get(`/`, (req, res) => res.render(`main`));
-app.get(`/register`, (req, res) => res.send(`/register`));
+app.get(`/register`, (req, res) => res.render(`sign-up`));
 app.get(`/login`, (req, res) => res.render(`login`));
 app.get(`/search`, (req, res) => res.send(`/search`));
 app.get(`/categories`, (req, res) => res.send(`/categories`));
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
+app.get(`/`, (req, res) => res.render(`main`));
 
 app.listen(DEFAULT_PORT);
