b0a92d06a47eb8823861af15f0b0a5a3781bb769
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-11-13 18:54:36 +0300

    Add templates for pages

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -7,7 +7,6 @@ articlesRoutes.get(`/`, (req, res) => res.send(`/articles`));
 articlesRoutes.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
 articlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
 articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+articlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
 
 module.exports = articlesRoutes;
-
