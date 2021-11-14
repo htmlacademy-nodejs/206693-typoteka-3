10ce13c8867bd7b4ec2fae8aef47fc586fcfd68b
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-11-14 07:13:28 +0300

    Add page render for articles-by-category

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 const articlesRoutes = new Router();
 
 articlesRoutes.get(`/`, (req, res) => res.send(`/articles`));
-articlesRoutes.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
+articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 articlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
 articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
 articlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
